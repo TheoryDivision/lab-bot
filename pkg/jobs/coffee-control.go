4149d6a3531f651869f530ffb6c062cd022cbe1b
Vishhvaan Gopalakrishnan <vish0288@gmail.com>
2022-06-25 09:44:44 -0400

    start raspberry pi gpio control

--- pkg/jobs/coffee-control.go
+++ pkg/jobs/coffee-control.go
@@ -1,1 +1,14 @@
 package jobs
+
+import (
+	"github.com/stianeikeland/go-rpio/v4"
+)
+
+func (cj *controllerJob) gpioInit() {
+	err := rpio.Open()
+	if err != nil {
+		cj.logger.WithField("error", err).Error("Cannot access GPIO")
+
+	}
+
+}
